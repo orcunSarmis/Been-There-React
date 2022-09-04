57e9c2b588e242b3d5030c6a820ea4cc9487f927
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-09-04 23:55:11 +1000

    avatar js and css added

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -3,12 +3,14 @@ import React from 'react';
 import UsersList from '../components/UsersList';
 
 const Users = () => {
-const USERS = [{
+const USERS = [
+    {
     id: 'u1', 
     name: 'Max P:ower', 
-    image: 'Photo by 3Motional Studio from Pexels: https://www.pexels.com/photo/fashion-photography-of-woman-hands-on-chin-with-glitter-makeup-1081685/', 
+    image: 'https://www.pexels.com/photo/fashion-photography-of-woman-hands-on-chin-with-glitter-makeup-1081685/', 
     places: 3
-}];
+    }
+];
 
     return <UsersList items={USERS}/>
 };
