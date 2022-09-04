9e72ba28fb17c085036d572a565b8d0742df7b6e
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-09-04 23:02:28 +1000

    actuall pages are working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { Routes,  Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 const App = () => {
   return (
     <React.Fragment>
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/places/new" element={<NewPlace />} />
+          <Route path="/*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
     </React.Fragment>
   );
-}
+};
 
 export default App;
