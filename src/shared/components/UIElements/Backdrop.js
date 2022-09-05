6bb44f6786adacbbc710d530257df2d3a22bcf14
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-09-05 10:56:20 +1000

    back drop added

---
+++ src/shared/components/UIElements/Backdrop.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { createPortal } from "react-dom";
+
+import './Backdrop.css';
+
+const Backdrop = props => {
+  return createPortal(
+    <div className="backdrop" onClick={props.onClick}></div>,
+    document.getElementById('backdrop-hook')
+  );
+};
+
+export default Backdrop;
