8c01f53ec2784d5f97084a947e892bfc8d41cf64
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-09-05 22:52:46 +1000

    error

--- src/shared/components/UIElements/Modal.js
+++ src/shared/components/UIElements/Modal.js
@@ -1,6 +1,7 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 import { createPortal } from "react-dom";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import { CSSTransition } from "react-transition-group";
 
 import Backdrop from "./Backdrop";
