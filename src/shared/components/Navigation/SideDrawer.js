a8155722c57962d5e4ed3c4700fc720fbad2260c
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-09-05 10:34:30 +1000

    cretae portall added

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,11 +1,14 @@
 import React from "react";
+import ReactDOM from 'react-dom/client';
+import { createPortal } from "react-dom";
 
 import './SideDrawer.css';
 
 const SideDrawer = props => {
-    return (
-        <aside className="side-drawer">{props.children}</aside>
-    );
+        const content = <aside className="side-drawer">{props.children}</aside>
+
+        return createPortal(content, document.getElementById('drawer-hook'));
+
 };
 
 export default SideDrawer;
