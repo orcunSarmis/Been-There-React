81b6a1018483267b9ecda310ab5f7519bf870f1a
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-09-17 22:27:22 +1000

    delete modal added

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -9,9 +9,24 @@ import "./PlaceItem.css";
 const PlaceItem = (props) => {
   const [showMap, setShowMap] = useState(false);
 
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+
   const openMapHandler = () => setShowMap(true);
 
   const closeMapHandler = () => setShowMap(false);
+
+  const showDeleteWarningHandler = () => {
+    setShowConfirmModal(true);
+  };
+
+  const cancelDeleteHandler = () => {
+    setShowConfirmModal(true);
+  };
+
+  const confirmDeleteHandler = () => {
+    setShowConfirmModal(false);
+    console.log('DELETING...');
+  };
 
   return (
     <React.Fragment>
@@ -26,6 +41,19 @@ const PlaceItem = (props) => {
           <Map center={props.coordinates} zoom={16} />
         </div>
       </Modal>
+      <Modal 
+      show={showConfirmModal}
+      onCancel={cancelDeleteHandler}
+      header="Are you sure?" 
+      footerClass="palce-item__modal-actions" 
+      footer={
+        <React.Fragment>
+          <Button inverse onClick={cancelDeleteHandler}>CANCEL</Button>
+          <Button inverse onClick={confirmDeleteHandler}>DELETE</Button>
+        </React.Fragment>
+      }>
+        <p>Do you want to delete this place?</p>
+      </Modal>
     <li className="palce-item">
       <Card className="palce-item__content">
         <div className="place-item__image">
@@ -39,7 +67,7 @@ const PlaceItem = (props) => {
         <div className="place-item__actions">
           <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
           <Button to={`/places/${props.id}`}>EDIT</Button>
-          <Button danger>DELETE</Button>
+          <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>
         </div>
       </Card>
     </li>
