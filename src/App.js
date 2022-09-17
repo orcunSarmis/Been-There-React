4e2759209f86b6fa0c868aa869a8f9a6bfb2831c
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-09-17 23:09:31 +1000

    auth added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlaces";
+import Auth from "./user/pages/Auth";
 import MainNvaigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
@@ -23,6 +24,7 @@ const App = () => {
             <Route path="/:userId/places"  element={<UserPlaces />}/>
             <Route path="/places/new" element={<NewPlace />} />
             <Route path="/places/:placeId" element={<UpdatePlace />} />
+            <Route path="/auth" element={<Auth />} />
             <Route path="/*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
